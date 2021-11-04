988df27e557bcca15e38bf3041ba63f930f8f8b3
Pedro Binotto <pedrosantibinotto@gmail.com>
2021-11-04 11:52:10 -0300

    --

--- linkedList.cpp
+++ linkedList.cpp
@@ -1,19 +1,8 @@
-#include <stdio.h>
 #include <stdlib.h>
-
-typedef struct node node_t;
-
-struct node {
-    int value;
-    node_t *next;
-};
+#include <stdio.h>
+#include <stdbool.h>
 
 int main(void) {
-    node_t n1;
-    n1.value = 13;
-    n1.next = NULL;
-    printf("%d\n", n1.value);
-
     return 0;
 }
 
