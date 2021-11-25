986acecb510a4115631fa899dd81004102d3c5bd
Pedro Binotto <pedrosantibinotto@gmail.com>
2021-11-24 22:52:13 -0300

    gerar arquivos iniciais

---
+++ 02lista_duplamente_encadeada/src/cursor.hpp
@@ -0,0 +1,11 @@
+#ifndef CURSOR_H
+#define CURSOR_H
+
+namespace dll {
+    class Cursor {
+
+    };
+}
+
+#endif
+
---
+++ 02lista_duplamente_encadeada/src/doublyLinkedList.hpp
@@ -0,0 +1,11 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+namespace dll {
+    class DoublyLinkedList {
+
+    };
+}
+
+#endif
+
